26c619e42d4e6a58ed3929df7666f275ff2b08aa
Maulana Sandy <maulanasandy.official@gmail.com>
2024-10-24 22:45:13 +0700

    Add files via upload

---
+++ asset/js/kalk.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteChar() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
+
+function appendDot() {
+    if (!display.value.includes('.')) {
+        display.value += '.';
+    }
+}
